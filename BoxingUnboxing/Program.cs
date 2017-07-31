a75acf6f80c622986613079cf6604b78c3be6f99
Justin Dietz <justin.r.dietz@gmail.com>
2017-07-31 14:11:47 -0700

    Boxing Unboxing

---
+++ BoxingUnboxing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> objList = new List<object>();
+            objList.Add(7);
+            objList.Add(28);
+            objList.Add(-1);
+            objList.Add(true);
+            objList.Add("chair");
+
+            int sum=0;
+            foreach (object item in objList)
+            {
+                if(item is int){
+                    sum+=(int)item;
+                }
+                Console.WriteLine(item);
+                
+            }
+            Console.WriteLine(sum);
+
+
+        }
+    }
+}
