e5f8ae75bc6aa27e21e7076a101b7fcd07420011
Justin Dietz <justin.r.dietz@gmail.com>
2017-08-03 21:30:01 -0700

    Includes optionals

--- music-linq/Program.cs
+++ music-linq/Program.cs
@@ -36,20 +36,33 @@ namespace ConsoleApplication
                 Console.WriteLine($"{a.ArtistName} - {a.Age}");
             }
             //(Optional) Display the Group Name of all groups that have members that are not from New York City
-            var groupsWithMemsNotFromNY = ( from g in Groups
-                                            from m in g.Members
-                                            where m.Age>5
-                                            select g);
+            var groupsWithMemsNotFromNY = ( from a in Artists
+                                            from g in Groups
+                                            where a.GroupId == g.Id && a.Hometown != "New York City"
+                                            select g).Distinct();
             System.Console.WriteLine("=======================================");
-            foreach (Group g in Groups)
+            foreach (Group g in groupsWithMemsNotFromNY)
             {
                 Console.WriteLine(g.GroupName);
-                foreach (Artist a in g.Members)
-                {
-                    Console.WriteLine($"\t{a.ArtistName} - {a.Hometown}");
-                }
+                // foreach (Artist a in g.Members)
+                // {
+                //     Console.WriteLine($"\t{a.ArtistName} - {a.Hometown}");
+                // }
             }
             //(Optional) Display the artist names of all members of the group 'Wu-Tang Clan'
+             var wuTangArtists = ( from a in Artists
+                                            from g in Groups
+                                            where a.GroupId == g.Id && g.GroupName == "Wu-Tang Clan"
+                                            select a);
+            System.Console.WriteLine("=======================================");
+            foreach (Artist a in wuTangArtists)
+            {
+                Console.WriteLine(a.ArtistName);
+                // foreach (Artist a in g.Members)
+                // {
+                //     Console.WriteLine($"\t{a.ArtistName} - {a.Hometown}");
+                // }
+            }
         }
     }
 }
